02451f96676de3ad4bbb205f2ed76eab5d6d8c7b
RayH-1 <59976416+RayH-1@users.noreply.github.com>
2025-05-08 17:12:35 +0200

    Updating the code for CELEX download, removing duplicate HTML files

--- text_processing/download_searches.py
+++ text_processing/download_searches.py
@@ -7,24 +7,25 @@ import os
 # The CELEX numbers are stored in a file 'searches.csv', which for example results from 
 # querying https://eur-lex.europa.eu/homepage.html for relevant policies.
 # Loading the file:
-# exp_search = 'text_processing/searches.csv'
-exp_search = 'CELEX_Renwables.csv'
-df = pd.read_csv(exp_search,encoding= "ISO-8859-1")
-
+exp_search_pre = 'text_processing/searches_2000-2019.csv'
+exp_search_post ='text_processing/searches_2020-2024.csv'
+df_pre = pd.read_csv(exp_search_pre,encoding= "ISO-8859-1")
+df_post = pd.read_csv(exp_search_post,encoding= "ISO-8859-1")
 
 # Function to take in a dataframe with CELEX numbers of EU laws and download the full text html file
 # Input: Data frame with CELEX numbers of EU laws
 # Output: Full text html files for each law, stored in the directory 'texts'
 
-def get_html(df):
-    os.mkdir('texts') 
+def get_html(df,output_path):
+    os.mkdir(f'texts/{output_path}') 
     for i in range(len(df)):
         celex= df.loc[i,'CELEX number']
         url = 'https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:{}&from=EN'.format(celex)
         resp = req.get(url)
         filename = '{}.html'.format(celex)
-        with open( 'texts/' + 'EU_' + filename, "w", encoding  = 'utf-8') as file:
+        with open( f'texts/{output_path}/' + 'EU_' + filename, "w", encoding  = 'utf-8') as file:
             file.write(resp.text,)
             file.close()
 
-get_html(df)
+get_html(df_pre, output_path='2000-2019')
+get_html(df_post, output_path='2020-2024')
